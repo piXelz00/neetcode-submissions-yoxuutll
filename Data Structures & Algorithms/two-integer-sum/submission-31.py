9d0f11dc05eceb4247b3d2d3fccd4de0b591c707
Rudra <89377265+piXelz00@users.noreply.github.com>
2026-04-10 16:47:53 +0530

    Add: two-integer-sum - submission-31

---
+++ Data Structures & Algorithms/two-integer-sum/submission-31.py
@@ -0,0 +1,57 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        set_nums = set(nums)
+        tw_sum = []
+        second = None
+        for i in range(0,len(nums)):
+            counter = 0
+            element = target - nums[i]
+            if element == nums[i]:                
+                counter = self.counter(0,i,nums)
+                
+            if second is None:
+                    if element in set_nums :
+                            if counter != 1:
+                                second = element 
+                                tw_sum.append(i)        
+            else:
+                        if  second == nums[i]:
+                            tw_sum.append(i)
+        return tw_sum
+
+
+    def counter(self,counter,n,nums):
+        counter = 0
+        for i in nums:
+            if i == nums[n]:
+                counter+=1
+        return counter  
+
+
+
+
+
+
+
+
+        
+
+
+                
+
+
+
+                
+                
+        
+
+                
+
+
+
+             
+        
+            
+            
+
