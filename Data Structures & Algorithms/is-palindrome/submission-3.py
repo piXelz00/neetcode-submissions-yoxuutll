3dd7eed0e4a7b437cbcd3cf5e158e51e97d8a1f8
Rudra <89377265+piXelz00@users.noreply.github.com>
2026-04-18 15:05:39 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+
+
+        left,right = 0,len(s)-1
+
+        while left<right:
+            if  not (48<=ord(s[left])<=57 or  65<=ord(s[left])<=90 or 97<=  ord(s[left])<= 122) :
+                left +=1
+            elif not (48<=ord(s[right])<=57 or  65<=ord(s[right])<=90 or 97<=  ord(s[right])<= 122) :
+                right-=1
+            else: 
+                if s[left].lower() !=s[right].lower():
+                    return False
+                left +=1
+                right-=1
+
+        return True        
+
+
+
+
+#class Solution2:
+#    def isPalindrome(self, s: str) -> bool:
+#        left, right = 0, len(s) - 1
+#        
+#        while left < right:
+#            # Move left pointer forward if it's not a letter/number
+#            if not s[left].isalnum():
+#                left += 1
+#            # Move right pointer backward if it's not a letter/number
+#            elif not s[right].isalnum():
+#                right -= 1
+#            # Both are alphanumeric, so compare them
+#            else:
+#                if s[left].lower() != s[right].lower():
+#                    return False
+#                left += 1
+#                right -= 1
+#                
+#        return True
+        
